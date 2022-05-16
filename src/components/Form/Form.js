971b36bb8576d5cd07ccd438e22066b7477fde26
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2022-05-16 14:51:01 +0200

    feat: add first name and last name to the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,6 +8,8 @@ import styles from './Form.module.scss';
 
 const Form = () => {
     const [emailError, setEmailError] = useState('');
+    const [FnameError, setFnameError] = useState('');
+    const [LnameError, setLnameError] = useState('');
     const { register, handleSubmit} = useForm();
 
     const onSubmit = () => {
@@ -16,27 +18,56 @@ const Form = () => {
 
     const validateEmail = (e) => {
         let email = e.target.value;
-        if (validator.isEmail(email) ){
+        if (validator.isEmail(email)){
             setEmailError('');
         } else {
             setEmailError('Enter valid Email!');
+        }
+
+    }
+    const validateFirstName = (e) =>{
+        let firstName = e.target.value;
+        if (validator.isEmpty(firstName)){
+            setFnameError(`The field shouldn't be empty`);
+        }
+        else {
+            setFnameError('')
+        }
+    }
+    const validateLastName = (e) =>{
+        let lastName = e.target.value;
+        if (validator.isEmpty(lastName)){
+            setLnameError(`The field shouldn't be empty`);
+        }
+        else {
+            setLnameError('')
         }
     }
 
     return (
         <form className={styles.Form} onSubmit={handleSubmit(onSubmit)}>
             <label>
+                <input type="text" id="userName" placeholder="First name" required={true}
+                       onChange={(e) => validateFirstName(e)}></input>
+                <span className={styles.emailError }>{FnameError}</span>
+            </label>
+            <label>
+                <input type="text" id="userSurame" placeholder="Last name" required={true}
+                       onChange={(e) => validateLastName(e)}></input>
+                <span className={styles.emailError }>{LnameError}</span>
+            </label>
+            <label>
                 <input type="email" id="userEmail" placeholder="Email" required={true} {...register("example")}
                        onChange={(e) => validateEmail(e)}></input>
                 <span className={styles.emailError }>{emailError}</span>
             </label>
+
             <button type="submit" className={styles.btnSubmit} >Read the blog post</button>
             <div>
             </div>
         </form>
 
     );
-    console.log(emailError);
 };
 
 Form.propTypes = {};
